8db053ab0079f48fef63df59cb376d43244f9d0e
小皮 <pphboy@qq.com>
2021-03-02 07:24:27 +0800

    初始化项目

---
+++ src/main/java/org/tomking/Tomking/TomkingMain.java
@@ -0,0 +1,13 @@
+package org.tomking.Tomking;
+
+/**
+ * Hello world!
+ *
+ */
+public class TomkingMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
