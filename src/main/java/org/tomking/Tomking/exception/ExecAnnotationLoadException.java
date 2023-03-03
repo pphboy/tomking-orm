7c18cd1f2f404d17509d4228db99a89b2ad50aaf
pphboy <pphboy@aliyun.com>
2023-03-03 23:22:08 +0800

    更新MapperFactor

---
+++ src/main/java/org/tomking/Tomking/exception/ExecAnnotationLoadException.java
@@ -0,0 +1,13 @@
+package org.tomking.Tomking.exception;
+
+public class ExecAnnotationLoadException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1657114139121117378L;
+	
+	public ExecAnnotationLoadException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/org/tomking/Tomking/factory/MapperFactory.java
@@ -0,0 +1,162 @@
+package org.tomking.Tomking.factory;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.Type;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.tomking.Tomking.Annotation.exec.Modify;
+import org.tomking.Tomking.Annotation.exec.Select;
+import org.tomking.Tomking.Annotation.mapper.Param;
+import org.tomking.Tomking.Utils.ExecuteSql;
+import org.tomking.Tomking.exception.ExecAnnotationLoadException;
+
+
+public class MapperFactory {
+	
+	/**
+	 * <h1> 数据库操作注解 </h1>
+	 * 存储着操作数据库的注解，Modify 和 Select 后面如果有的话会再加
+	 * 因为这判断这两个，如果都没有的话，或者超过了，需要报错
+	 */
+	List<Class<? extends Annotation>> execAnnotationlist = Arrays.asList(Modify.class,Select.class);
+
+
+	/**
+	 * 获取Mapper映射类的实例
+	 * @param <V>
+	 * @param instanceClass
+	 * @return
+	 */
+	public <V> V getMapperInstance(Class instanceClass) {
+		System.out.println(instanceClass.getInterfaces());
+        Object obj = Proxy.newProxyInstance(instanceClass.getClassLoader(),new Class[]{instanceClass}, new InvocationHandler() {
+        	/**
+        	 * 这个方法是指在Mapper接口调用方法的时候，生成的一个代理。
+        	 */
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                
+				/*将以Param命名的变量的值 于Param的值对应起来*/
+            	// 每个方法都需要对应一个键值绑定
+				Map<String,Object> map = new HashMap<>(); // map的创建与否取决于该类是不是一个对象，或者是有对象的
+				
+
+                Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+				// 在这里直接生成一个KeyValue的数组，绑定好数据和占位符的值
+				// 获取Param，并且将Param和 SQL 的参数对上，如果对不上则报错
+                for (int i = 0; i < method.getParameters().length; i++) {
+                    for(Annotation annotation: method.getParameters()[i].getAnnotations()){
+
+                        System.out.println(annotation);
+                        if(annotation instanceof Param){
+                            Param param = (Param) annotation;
+                            /*将参数的值添加以Key:Value形式添加到内*/
+                            map.put(param.value(),args[i]);
+                            System.out.println("value:"+param.value() +" arg: "+args[i]);
+                        }
+                    }
+                }
+
+                // 判断 Annotation的数量，Exec的数量必须等于1
+                List<Annotation> annotationList = new ArrayList();
+
+                // 把这个操作数据库的注解全部拿出来
+                for(Class tmpAnno: execAnnotationlist) {
+                	Annotation tmp = method.getAnnotation(tmpAnno);
+                	if(tmp != null) {
+						annotationList.add(tmp);
+                	}
+                }
+                // 如果没有写注解，直接返回空即可
+                if(annotationList.size() == 0) {
+                	return null;
+                }
+                // exec包下的 注解不能超过一个，目前仅有 Select 和 Modify，如果超过一个则是无法理解
+                if( annotationList.size() > 1) {
+                	throw new ExecAnnotationLoadException(method.getDeclaringClass().getName()+"."+method.getName()+"注解不能过超过两个");
+                } 
+                
+                // 因为Mapper的方法只会存在一个Exec的注解
+                Annotation currentAnnotation = annotationList.get(0);
+//				Class annotationClassType = currentAnnotation.annotationType();
+
+				if(currentAnnotation instanceof  Modify) {
+					return modify(method,args,map);
+				}else if(currentAnnotation instanceof Select) {
+					return select(method,args,map);
+				}
+				
+				// 如果即不是修改也不是查询，则返回Null
+				return null;
+
+            }
+        });
+        
+        return (V)obj;
+	}
+	/**
+	 * 插入、删除、更新，都是归为此 modify方法
+	 * 如果是传入对象，需要匹配其名称和对应的键，并把 值映射的位置做出来，然后插入到 列表内，再使用prepared
+	 * @param method
+	 * @param args
+	 * @param paramArgMap
+	 * @return
+	 */
+	public Object modify(Method method,Object[] args,Map<String,Object> paramArgMap) {
+		Modify modify = (Modify)method.getAnnotation(Modify.class);
+		System.out.println("Modify Value: "+modify.value());
+		
+		return null;
+	}
+	
+	/**
+	 * 查询操作
+	 * @param method
+	 * @param args
+	 * @param paramArgMap
+	 * @return
+	 * @throws ClassNotFoundException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchFieldException
+	 * @throws InstantiationException
+	 * @throws SQLException
+	 */
+	public Object select(Method method,Object[] args,Map<String,Object> paramArgMap) throws ClassNotFoundException, IllegalAccessException, NoSuchFieldException, InstantiationException, SQLException {
+		/*获取查询语句的值*/
+		Select select = (Select)method.getAnnotation(Select.class);
+		System.out.println(select.value());
+		/*返回的类型*/
+		Type genericReturnType = method.getGenericReturnType();
+		Class<?> generic = null;
+		/*获取List泛型的class*/
+		if(genericReturnType instanceof  ParameterizedType){
+			ParameterizedType pt = (ParameterizedType) genericReturnType;
+			generic =  Class.forName(pt.getActualTypeArguments()[0].getTypeName());
+			System.out.println(generic);
+		}
+		System.out.println(paramArgMap);
+		return ExecuteSql.select(select.value(),paramArgMap,generic);
+	}
+	
+	/**
+	 * 
+	 * @param method mapper方法的method
+	 * @param args 参数列表
+	 * @return
+	 */
+	public Map<String,Object> bindingParamAndSqlPlaceholder(Method method, Object[] args){
+		Map<String,Object> map = new HashMap();
+		
+		return map;
+	}
+	
+}
